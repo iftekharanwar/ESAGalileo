979a7c2cff4303537d492c24f386cf085ce06bbd
iftekharanwar <anwariftekhar8@gmail.com>
2024-05-10 12:04:36 +0000

    Fix API URL query parameter syntax in satellite-data.js

--- netlify/functions/satellite-data.js
+++ netlify/functions/satellite-data.js
@@ -12,7 +12,7 @@ exports.handler = async function(event, context) {
     };
   }
 
-  const apiUrl = `https://api.n2yo.com/rest/v1/satellite/above/41.702/-76.014/0/70/18/&apiKey=${process.env.REACT_APP_N2YO_API_KEY}`;
+  const apiUrl = `https://api.n2yo.com/rest/v1/satellite/above/41.702/-76.014/0/70/18/?apiKey=${process.env.REACT_APP_N2YO_API_KEY}`;
 
   try {
     const response = await fetch(apiUrl);
